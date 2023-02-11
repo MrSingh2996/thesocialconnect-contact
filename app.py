579ad41442d6d2630133c8a515bbc2d79de09a87
TheSocialConnect <socialconnect23@gmail.com>
2023-02-12 01:53:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,33 +27,68 @@ def contact():
     # cursor.execute("INSERT INTO contacts (name, email, message) VALUES (%s, %s, %s)", (name, email, message))
     # conn.commit()
 
-    # if request.form.get("send_response") == "yes":
-    msg = MIMEText("Thank you for contacting us. We will get back to you soon.")
-    msg['Subject'] = 'Contact Form Submission'
-    msg['From'] = 'socialconnect23@gmail.com'
-    msg['To'] = email
-    recipients = [email, "ocialconnect23@gmail.com"]
+
+
+    # Set up the SMTP connection
+    smtp_server = 'mtp.gmail.com'
+    smtp_port = 587  # or the port your SMTP server uses
+    smtp_username = 'socialconnect23@gmail.com'
+    smtp_password = 'aewfewssqbnhtdui'
+    smtp_conn = smtplib.SMTP(smtp_server, smtp_port)
+    smtp_conn.starttls()
+    smtp_conn.login(smtp_username, smtp_password)
+
+    # Create the message
+    sender = 'socialconnect23@gmail.com'
+    recipients = ['sociallearno@gmail.com', 'vspsp111@gmail.com']
+    subject = 'New Enquiry'
+    body = 'One more enquiry raised.'
+
+    # Format the message headers and body
+    message = f"From: {sender}\nTo: {', '.join(recipients)}\nSubject: {subject}\n\n{body}"
+
+    # Send the message
+    smtp_conn.sendmail(sender, recipients, message)
+
+    # Close the SMTP connection
+    smtp_conn.quit()
+
+
+
+
+    if request.form.get("send_response") == "yes":
+        msg = MIMEText("Thank you for contacting us. We will get back to you soon.")
+        msg['Subject'] = 'Contact Form Submission'
+        msg['From'] = 'socialconnect23@gmail.com'
+        msg['To'] = email
         # create an instance of the SMTP class
-    server = smtplib.SMTP('smtp.gmail.com', 587)
+        server = smtplib.SMTP('smtp.gmail.com', 587)
 
         # start the TLS encryption
-    server.starttls()
+        server.starttls()
 
         # login to the server using your email and password
-    server.login("socialconnect23@gmail.com", "aewfewssqbnhtdui")
+        server.login("socialconnect23@gmail.com", "aewfewssqbnhtdui")
 
         # send the email to the recipient
-    server.sendmail('socialconnect23@gmail.com', recipients, msg.as_string())
-    server.quit()
-    message = 'Your message has been sent successfully!'
+        server.sendmail('socialconnect23@gmail.com', email, msg.as_string())
+        server.quit()
+        message = 'Your message has been sent successfully!'
 
-    return render_template('index.html', message=message)
-    # else:
-    #     message = 'Your message has been sent successfully!'
-    #     return render_template('index.html', message=message)
-    
-    # return render_template('index.html')
+        return render_template('index.html', message=message)
+    else:
+        message = 'Your message has been sent successfully!'
+        return render_template('index.html', message=message)
+    return render_template('index.html')
     
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=1029)
+
+
+
+
+
+
+
+
