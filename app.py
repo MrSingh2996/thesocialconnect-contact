0e4c22d95ceb557653e2c0ab057ca76b3b5cd4fb
TheSocialConnect <socialconnect23@gmail.com>
2023-02-12 00:31:59 +0530

    now

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, request, render_template
+# import mysql.connector
+import smtplib
+from email.mime.text import MIMEText
+
+app = Flask(__name__, static_folder='static')
+
+# conn = mysql.connector.connect(
+#   host="localhost",
+#   port="3306",
+#   user="root",
+#   password="Vikram@1029",
+#   database="contactform"
+# )
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/contact', methods=['POST'])
+def contact():
+    message = ''
+    name = request.form['name']
+    email = request.form['email']
+    message = request.form['message']
+    # cursor = conn.cursor()
+    # cursor.execute("INSERT INTO contacts (name, email, message) VALUES (%s, %s, %s)", (name, email, message))
+    # conn.commit()
+
+    if request.form.get("send_response") == "yes":
+        msg = MIMEText("Thank you for contacting us. We will get back to you soon.")
+        msg['Subject'] = 'Contact Form Submission'
+        msg['From'] = 'rude8032@gmail.com'
+        msg['To'] = email
+        # create an instance of the SMTP class
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+
+        # start the TLS encryption
+        server.starttls()
+
+        # login to the server using your email and password
+        server.login("rude8032@gmail.com", "chxxslrlszocvkra")
+
+        # send the email to the recipient
+        server.sendmail('rude8032@gmail.com', email, msg.as_string())
+        server.quit()
+        message = 'Your message has been sent successfully!'
+
+        return render_template('index.html', message=message)
+    else:
+        message = 'Your message has been sent successfully!'
+        return render_template('index.html', message=message)
+    
+    return render_template('index.html')
+    
+
+if __name__ == '__main__':
+    app.run()
